f181e81c8df23cc6bc30828584b17bcc6ba3d277
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-05-13 13:00:19 +0600

    dfbgt

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,3 +1,4 @@
-const baseUrl = "https://swe-4602-lab.azurewebsites.net";
+// const baseUrl = "https://swe-4602-lab.azurewebsites.net";
+const baseUrl = "http://localhost:3000";
 
 export default baseUrl;
