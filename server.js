04930403c5f2dfb406e6b3a6ffc76e66cb67b51d
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-05-13 12:14:45 +0600

    server changed

--- server.js
+++ server.js
@@ -45,3 +45,11 @@ app.post("/create", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on post ${port}`);
 });
+
+const path = require('path')
+   
+    //production script
+    app.use(express.static("./client/build"))
+    app.get("*", (req,res)=>{
+      res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+        })
